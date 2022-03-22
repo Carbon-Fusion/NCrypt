d4f70a5843168ff4d4837239b1462ba1ddcf0277
Utkarsh Tiwari <utkarsh02t@gmail.com>
2022-03-22 19:02:39 +0530

    Revert "Add applocalization to fix flutter test"
    This reverts commit 8c61dc86ced70917d0b7a0867518dfbf71c31d16.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,13 +12,7 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(
-      const MaterialApp(
-        localizationsDelegates: AppLocalizations.localizationsDelegates,
-        supportedLocales: AppLocalizations.supportedLocales,
-        home: MyApp(),
-      ),
-    );;
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
